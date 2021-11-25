6fb34a739a34be42c16ecba1b2d7db623ff27056
daedInside <91154979+daedInside@users.noreply.github.com>
2021-11-25 22:52:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main()
+{
+	int  z, x, c, v;
+   	printf("Enter chislo v mm:");
+	scanf_s("%d", &x);
+	z = x / 1000;
+	printf("m:%d\n", z);
+	c = x / 10;
+	printf("cm:%d\n", c);
+	printf("mm:%d", x);
+
+	return 0;
+}
